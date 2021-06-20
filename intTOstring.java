53c323e96ebeb9910c131698c922a48cb52010ff
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-20 21:49:05 +0530

    DAY2 - question4

---
+++ intTOstring.java
@@ -0,0 +1,14 @@
+public class intTOstring {
+    public static void main(String[] args) {
+        int num = 3456;
+        //USING String.valueOf()
+        String str = String.valueOf(num);
+        //USING Integer.toString()
+        String str1 = Integer.toString(num);
+
+        System.out.println(str);
+        System.out.println(str1);
+
+    }
+    
+}
