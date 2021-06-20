601a0828e0d79b32b54c3c80a447a3839c60e26f
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-20 21:53:00 +0530

    .

---
+++ stringTOlong.java
@@ -0,0 +1,10 @@
+public class stringTOlong {
+    public static void main(String[] args) {
+        String str = "872349852323987324";
+
+        long l = Long.parseLong(str);
+
+        System.out.println(l);
+    }
+    
+}
