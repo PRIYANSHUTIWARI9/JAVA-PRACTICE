e48eb65d02d99ee60c8b17616c4c7db3753f6359
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-20 20:19:36 +0530

    DAY2 - question2

---
+++ permutationOfString.java
@@ -0,0 +1,39 @@
+public class permutationOfString {
+
+    public static String swapString(String a, int i, int j)
+    {
+        char[] b=a.toCharArray();
+        char ch;
+        ch = b[i];
+        b[i] = b[j];
+        b[j] = ch;
+
+        return String.valueOf(b);
+    }
+    public static void main(String[] args) {
+        String str = "ABC";
+        int len = str.length();
+        System.out.println("all the permutation of String are:- ");
+        generatePermutation(str,0,len);
+
+    }
+    public static void generatePermutation(String str, int start, int end)
+    {
+        if(start == end -1)
+        {
+            System.out.println(str);
+        }
+        else
+        {
+            for(int i=start; i<end; i++)
+            {
+                str = swapString(str,start,i);
+
+                generatePermutation(str, start+1, end);
+
+                str = swapString(str,start,i);
+            }
+        }
+    }
+    
+}
