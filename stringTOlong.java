9b2ca145373a10588d5fc0122440d0d65e55b1c1
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-20 21:56:33 +0530

    DAY2 - question5

--- stringTOlong.java
+++ stringTOlong.java
@@ -1,6 +1,6 @@
 public class stringTOlong {
     public static void main(String[] args) {
-        String str = "872349852323987324";
+        String str = "87234985232398734";
 
         long l = Long.parseLong(str);
 
