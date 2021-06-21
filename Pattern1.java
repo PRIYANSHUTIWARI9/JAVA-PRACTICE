24be54544f621a5cd7e50a647d7e2499d6ec96d9
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-21 22:35:16 +0530

    DAY3 - question1

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        int row = 9;
+        
+        for(int i=1; i<=row; i++)
+        {
+            int num=i;
+            for(int j=1 ; j<=i; j++)
+            {
+                System.out.print(num+" ");
+                num = num + row-j;
+            }
+            System.out.println();
+        }
+    }
+    
+}
