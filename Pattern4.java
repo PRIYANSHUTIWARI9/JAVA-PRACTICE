c4138cfdc3d7b8549299a0b171373eea1a27f8e9
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-21 23:20:18 +0530

    DAY3 - question4

---
+++ Pattern4.java
@@ -0,0 +1,15 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        int alpha = 65;
+        int row=8;
+        for(int i =0; i<=row; i++)
+        {
+            for(int j=0; j<=i; j++)
+            {
+                System.out.print((char)(alpha+j));
+            }
+            System.out.println();
+        }
+    }
+    
+}
