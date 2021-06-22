ff5d6293433479a691fdf4c35a3f7f12c8e8da3a
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-22 12:32:44 +0530

    Ascii value of two characters

---
+++ DAY4_question4.java
@@ -0,0 +1,8 @@
+public class DAY4_question4 {
+    public static void main(String[] args) {
+        int ch = 'a';
+        int ch1 = 'A';
+        System.out.println("the ascii value of ch & ch1 are :- "+ (ch)+" $ "+(ch1));
+        
+    }
+}
