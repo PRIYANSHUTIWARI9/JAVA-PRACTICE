baf985d1b2b6c62499831a6e37974a381a88064b
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-22 12:44:16 +0530

    total ascii number with their values

---
+++ DAY4_question5.java
@@ -0,0 +1,15 @@
+public class DAY4_question5 {
+    public static void main(String[] args) {
+        int alpha =65;
+        int alpha1 = 97;
+        for(int i=alpha;i<=alpha+25;i++)
+        {
+            System.out.print((char)i+" ");
+        }
+        System.out.println();
+        for(int i=alpha1;i<=alpha1+25;i++)
+        {
+            System.out.print((char)i+" ");
+        }
+    }   
+}
