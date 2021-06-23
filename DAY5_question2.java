7893827fd4954faeb172cfb8b272a139b3ccf694
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-06-23 22:57:22 +0530

    pattern9

---
+++ DAY5_question2.java
@@ -0,0 +1,19 @@
+public class DAY5_question2 {
+   public static void main(String[] args) {
+    int row = 8;
+    for(int i=1; i<=row; i++)
+    {
+        for(int j=i; j<=row; j++)
+        {
+            System.out.print(" ");
+        }
+        for(int k=i;k<=i;k++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+        
+    }
+   }
+    
+}
