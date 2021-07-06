d0c5c2afdd71d0f38eb7a72075d561ba63413ac7
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-07-06 23:44:01 +0530

    binary search

---
+++ searching/binary_search1.java
@@ -0,0 +1,20 @@
+package searching;
+
+import java.util.Arrays;
+
+public class binary_search1 {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};
+        int key = 30;
+        int result = Arrays.binarySearch(arr, key);
+        if(result < 0)
+        {
+            System.out.println("Element is not found !");
+        }
+        else
+        {
+            System.out.println("element is  found at index: "+result);
+        }
+    }
+    
+}
