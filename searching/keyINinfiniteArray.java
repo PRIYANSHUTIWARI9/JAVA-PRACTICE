0fd4529c1d09c2f9c23f0ae129bfe1bf8bf214bb
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-07-08 00:36:43 +0530

    wrong

--- searching/keyINinfiniteArray.java
+++ searching/keyINinfiniteArray.java
@@ -1,21 +1,22 @@
 package searching;
 
 public class keyINinfiniteArray {
-    public static int searchInfinite(int arr[] , int key)
-    {
-        int low =0;
-        int high = 1;
-        while(arr[high]< key)
-        {
-            low = high;
-            high = 2*high;
-        }
+    // public static int searchInfinite(int arr[] , int key)
+    // {
+    //     int low =0;
+    //     int high = 1;
+    //     while(arr[high]< key)
+    //     {
+    //         low = high;
+    //         high = 2*high;
+    //     }
 
-        return binarySearch(arr, low, high, key);
-    }
+    //     return binarySearch(arr, low, high, key);
+    // }
+   
     public static int binarySearch(int arr[], int low, int high, int key)
     {
-        int mid = (low+high)/2;
+        int mid = (low+(high-low))/2;
         while(low<high)
         {
             if(arr[mid] == key)
@@ -24,20 +25,23 @@ public class keyINinfiniteArray {
         }
         else if(arr[mid]> key)
         {
-            low= mid+1;
+            return binarySearch(arr, low, mid-1, key);
         }
         else 
         {
-            high = mid-1;
+            return binarySearch(arr, mid+1, high, key);
         }
         }
         return -1;
     }
+
     public static void main(String[] args) {
         
-        int[] arr = {12,34,23,32,5,42,24,56,67,32};
+        int[] arr = new int[]{12,34,23,32,5,42,24,56,67,32};
         int key = 67;
-        int result = searchInfinite(arr, key);
+        int low = 0;
+        int high = arr.length-1;
+        int result = binarySearch(arr, low, high, key);
 
         if(result == -1)
         {
@@ -47,9 +51,14 @@ public class keyINinfiniteArray {
         {
             System.out.println("element found at index" + result);
         }
-        
-
 
     }
     
 }
+
+
+
+     
+    
+ 
+  
